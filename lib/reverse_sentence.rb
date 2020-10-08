9d51ede16f10f7303a94b4a826efb4f61a78ccc1
Li Dai <li.lea.dai@gmail.com>
2020-10-08 01:08:33 -0700

    updated reverse_sentence and sort_by_length methods. Needs to confirm test case for reverse_sentence

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,21 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return "" if my_sentence.nil?
+  my_array = my_sentence.split(/\s/)
+    i = 0
+    j = my_array.length - 1
+
+    while i < j
+      temp = my_array[i]
+      my_array[i] = my_array[j]
+      my_array[j] = temp
+
+      i += 1
+      j -= 1
+    end
+
+  return my_array.join(' ')
 end
+
+
