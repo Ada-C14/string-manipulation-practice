cd6759ac4a3e5a501b57e3a707e516036338dc65
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-10-10 13:57:21 -0700

    add sort_by_length method

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,19 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  return [] if my_sentence.length == 0
+  string_to_array = my_sentence.split
+  if string_to_array.count < 2
+    return string_to_array
+  else
+    pivot = string_to_array[0]
+    less = Array.new
+    greater = Array.new
+    string_to_array[1..string_to_array.count].each do |word|
+      less.push(word) if word.length < pivot.length
+      greater.push(word) if word.length >= pivot.length
+    end
+  end
+  return sort_by_length(less.join(" ")) + [pivot] + sort_by_length(greater.join(" "))
+  # raise NotImplementedError, "Method not implemented"
 end
