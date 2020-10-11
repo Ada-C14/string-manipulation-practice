014474afd9729b5178cf93031edb62d78d4a4b9c
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-10-10 21:55:51 -0700

    explain time/space complexity

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,10 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n): there are two loops, first loop is O(n/2) and the second loop is O(n).
+#                        So, O(n/2) + O(n) --> O(n)
+# Space complexity: O(1): In the first loop on line 16, it reverses the string in place.
+#                         In the second loop on line 24, the helper method "reverse_a_word" reverses each word in place,
+#                         and the reversed word replaces the original String[x..y], it's in place replacement.
+#                         Plus extra variables, overall, the space complexity is a constant.
 
 def reverse_sentence(my_sentence)
   return "" if my_sentence.nil? || my_sentence.empty?
@@ -17,8 +21,8 @@ def reverse_sentence(my_sentence)
     # Step 2: Reverse each word in the String back to its original word
     start_idx = 0
     end_idx = 0
-    while end_idx < (my_sentence.length-1)
-      # call the helper method reverse_a_word to reverse each word
+    while end_idx < (my_sentence.length - 1)
+      # call the helper method reverse_a_word to reverse the words
       if my_sentence[end_idx].match?(/\s/)
         # when the first element in the string is not a whitespace
         if end_idx > 0
@@ -39,7 +43,7 @@ def reverse_sentence(my_sentence)
   end
 end
 
-# Create a helper method
+# Create a helper method to reverse the reversed word
 def reverse_a_word(word)
   idx = 0
   while idx < (word.length / 2)
@@ -49,25 +53,28 @@ def reverse_a_word(word)
   return word
 end
 
-### Reverse the words in a String by using Array
+### Reverse the String by using Array
+# Time complexity: O(n): one loop depends on the string length, the time complexity is O(n)
+# Space complexity: O(n): the split method used for the variable "re_sentence" generates a O(n) space complexity.
+#                         Plus extra variables, the space complexity is O(n)
+
 # def reverse_sentence(my_sentence)
 #   return "" if my_sentence.nil? || my_sentence.empty?
 #   if my_sentence.split.length == 1
 #     return my_sentence
 #   else
-#     re_sentence = my_sentence.gsub!(/\s/, "*").split(/(\*+)/)
-#     idx = 0
-#     while idx < (re_sentence.count / 2)
-#       re_sentence[idx], re_sentence[re_sentence.count - idx - 1] = re_sentence[re_sentence.count - idx - 1], re_sentence[idx]
-#       idx += 1
-#     end
+#     re_sentence = my_sentence.split(/(\s+)/)
 #
-#     final_sentence = re_sentence.map do |word|
-#       if word.match? (/(\*+)/)
-#         word = " " * word.length
-#       end
-#       word
+#     idx = re_sentence.length - 1
+#     start_idx = 0
+#     end_idx = 0
+#     while idx >= 0
+#       end_idx += re_sentence[idx].length - 1
+#       my_sentence[start_idx..end_idx] = re_sentence[idx]
+#       start_idx = end_idx + 1
+#       end_idx += 1
+#       idx -= 1
 #     end
-#     return final_sentence.join
+#     return my_sentence
 #   end
 # end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,10 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2): the looping time for each recursion is descending,
+#                          so the total time would be ((1 + n) * n/2 --> O(n^2)
+# Space complexity: O(n^2): for each recursion, it creates a set of string_to_array/less/greater arrays (O(3n)).
+#                         if there are (n-1) times of recursion, the space complexity would be O(3n)*O(n-1) --> O(n^2)
+
 def sort_by_length(my_sentence)
   return [] if my_sentence.length == 0
   string_to_array = my_sentence.split
@@ -19,3 +22,5 @@ def sort_by_length(my_sentence)
   return sort_by_length(less.join(" ")) + [pivot] + sort_by_length(greater.join(" "))
   # raise NotImplementedError, "Method not implemented"
 end
+
+
