c0b0bddba61ac74a304ece76bcc59ed388f9f409
Alice Boone <alicegbonel@gmail.com>
2020-10-10 20:52:19 -0700

    Wave 1 - sort_by_length.rb

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,19 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array_of_word = my_sentence.split(" ")
+  i = 0
+  while i < array_of_word.length - 1
+    j = 0
+    while j < array_of_word.length - i - 1
+      if array_of_word[j].length > array_of_word[j+1].length
+        temp = array_of_word[j]
+        array_of_word[j] = array_of_word[j + 1]
+        array_of_word[j + 1] = temp
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return array_of_word
 end
