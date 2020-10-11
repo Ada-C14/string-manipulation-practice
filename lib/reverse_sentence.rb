4493d6c869f5fcd7247d592873f573b241e90b25
roshni-patel <rpatel01@wesleyan.edu>
2020-10-11 11:22:05 -0500

    Wrote reverse_sentence method and added space and time complexities

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,24 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because there is one loop that depends on the size of my_sentence
+# Space complexity: O(n) because the size of the words array depends on the size of my_sentence
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if my_sentence.nil? || my_sentence.empty?
+    return my_sentence
+  end
+
+  words = my_sentence.split(/(?=\s)|(?<=\s)/)
+
+  first = 0
+  last = words.length - 1
+
+  while first < last
+    temp = words[first]
+    words[first] = words[last]
+    words[last] = temp
+    first += 1
+    last -= 1
+  end
+
+  my_sentence[0...my_sentence.length] = words.join[0...my_sentence.length]
+  return my_sentence
 end
