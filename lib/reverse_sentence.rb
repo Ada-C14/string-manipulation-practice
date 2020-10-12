d328866a4a8b44376b1d339c1aedc24b10f60b25
dnsrocha <drocha@gmail.com>
2020-10-12 12:04:54 -0700

    wave 2

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,47 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence.nil?
+
+  reverse_phrase = []
+  word = []
+  index = 0
+
+  while index < my_sentence.length
+    if my_sentence[index] == " "
+      reverse_phrase << word.join
+      reverse_phrase << " "
+      word = []
+    else
+      word << my_sentence[index]
+    end
+    index += 1
+  end
+  reverse_phrase << word.join
+
+  last_index = reverse_phrase.length - 1
+  index = 0
+  while last_index >= 0
+    if index < last_index
+      word = reverse_phrase[index]
+      reverse_phrase[index] = reverse_phrase[last_index]
+      reverse_phrase[last_index] = word
+      index += 1
+    end
+    last_index -= 1
+  end
+
+base_index = 0
+sentence = reverse_phrase.join
+while base_index < sentence.length
+  my_sentence[base_index] = sentence[base_index]
+  base_index += 1
 end
+  return my_sentence
+end
+
+
+
