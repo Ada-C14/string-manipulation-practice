17d1c91cb202b580a204436f2b1c52876f792e7b
India Kato <india.kato@gmail.com>
2020-10-12 15:41:00 -0700

    reverse sentence and sort by length methods

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,21 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if !my_sentence
+  split_sentence = my_sentence.split(/( +)/)
+  i = 1
+  while i <= split_sentence.length/2
+    temp = split_sentence[i-1]
+    split_sentence[i-1] = split_sentence[split_sentence.length-i]
+    split_sentence[split_sentence.length-i] = temp
+    i+=1
+  end
+  temp_string = split_sentence.join
+
+  j = 0
+  while j < temp_string.length
+    my_sentence[j] = temp_string[j]
+    j += 1
+  end
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,18 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(1)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  sentence_array = my_sentence.split(" ")
+  sentence_array.length.times do
+    i = 0
+    sentence_array.each do |string|
+      if string.length > sentence_array[i + 1].length
+        sentence_array[i], sentence_array[i + 1] = sentence_array[i + 1], sentence_array[i]
+      end
+      i += 1 if i < sentence_array.length - 2
+    end
+  end
+  return sentence_array
+
 end
