be458fea3b090af48ab3a472bf17af24f1337a1b
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-10-11 23:09:39 -0700

    add merge sort

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -2,33 +2,53 @@
 #  sorted by the length of the word.
 
 
-############ Insertion sort ############
+############ Merge sort ############
 ### Works for maintaining the order they appeared in the original string ###
 #
-# Time complexity: O(n^2): there are two while loops, and the inner one is nested in the outer one.
-#                          When the inner one compares to the front indexes to do the insertion, it takes O(n),
-#                          and the outer loop runs the whole String, ie O(n).
-#                          so the time complexity is O(n) * O(n) --> O(n^2)
+# Time complexity: O(nlogn): there are three steps for merge sort.
+#                          The helper method "divide_sort_array" divides an array into 2 sub-arrays everytime and sorts them when there are less then 2 elements in a sub-array.
+#                          Its time complexity is O(logn).
+#                          The helper method "merge_array" merges the sub-arrays and it depends on the length of the array, ie O(n).
+#                          So the overall time complexity is O(logn) * O(n) --> O(nlogn)
 # Space complexity: O(n): the variable "string_to_array" depends on the length of the String, it is O(n).
-#                         The inner loop is in-place swap which doesn't take extra space.
-#                         So plus the extra variables as constant, the space complexity is still O(n)
+#                         In "divide_sort_array", it also generates new arrays, lower_index & higher_index, which are depending on the length of the array, it is O(n)
+#                         Merge method uses the space depends on the length of the array, it is O(n).
+#                         Plus the extra variables as constant, the space complexity is O(n) + O(n) + O(n) + O(1) -->O(n)
 def sort_by_length(my_sentence)
-  return [] if my_sentence.length == 0
   string_to_array = my_sentence.split
-  if string_to_array.count > 1
-    idx = 0
-    while idx < (string_to_array.length - 1)
-      inner_idx = idx + 1
-      while inner_idx > 0
-        if string_to_array[inner_idx].length < string_to_array[inner_idx - 1].length
-          string_to_array[inner_idx], string_to_array[inner_idx - 1] = string_to_array[inner_idx - 1], string_to_array[inner_idx]
-        end
-        inner_idx -= 1
-      end
-      idx += 1
+  return divide_sort_array(string_to_array)
+end
+
+# Helper method 1 - divide and sort an array
+def divide_sort_array(array)
+  if array.count < 2
+    return array
+  else
+    mid_point = array.count / 2
+    lower_index = array[0...mid_point]
+    lower_array = divide_sort_array(lower_index)
+    higher_index = array[mid_point...array.count]
+    higher_array= divide_sort_array(higher_index)
+    return merge_array(lower_array, higher_array)
+  end
+end
+
+# Helper method 2 - marge the array
+def merge_array(array1, array2)
+  merge = Array.new
+  array1_idx = 0
+  array2_idx = 0
+  while merge.count < (array1.count + array2.count)
+    return merge + array2[array2_idx...array2.count] if array1[array1_idx].nil?
+    return merge + array1[array1_idx...array1.count] if array2[array2_idx].nil?
+    if array1[array1_idx].length <= array2[array2_idx].length
+      merge.push(array1[array1_idx])
+      array1_idx += 1
+    else
+      merge.push(array2[array2_idx])
+      array2_idx += 1
     end
   end
-  return string_to_array
 end
 
 
@@ -123,3 +143,32 @@ end
 #   return string_to_array
 # end
 
+
+############ Insertion sort ############
+### Works for maintaining the order they appeared in the original string ###
+#
+# Time complexity: O(n^2): there are two while loops, and the inner one is nested in the outer one.
+#                          When the inner one compares to the front indexes to do the insertion, it takes O(n),
+#                          and the outer loop runs the whole String, ie O(n).
+#                          so the time complexity is O(n) * O(n) --> O(n^2)
+# Space complexity: O(n): the variable "string_to_array" depends on the length of the String, it is O(n).
+#                         The inner loop is in-place swap which doesn't take extra space.
+#                         So plus the extra variables as constant, the space complexity is still O(n)
+# def sort_by_length(my_sentence)
+#   return [] if my_sentence.length == 0
+#   string_to_array = my_sentence.split
+#   if string_to_array.count > 1
+#     idx = 0
+#     while idx < (string_to_array.length - 1)
+#       inner_idx = idx + 1
+#       while inner_idx > 0
+#         if string_to_array[inner_idx].length < string_to_array[inner_idx - 1].length
+#           string_to_array[inner_idx], string_to_array[inner_idx - 1] = string_to_array[inner_idx - 1], string_to_array[inner_idx]
+#         end
+#         inner_idx -= 1
+#       end
+#       idx += 1
+#     end
+#   end
+#   return string_to_array
+# end
