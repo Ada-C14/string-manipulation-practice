b2eadbfe5d173cf730dd8ddf4b87c8a2e4468d47
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-10-12 15:27:35 -0700

    all tests passing

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,20 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence.nil?
+  array = my_sentence.split(/ /,-1)
+  return my_sentence if my_sentence.split(/ /,-1).length < 2
+
+  i = 0
+  j = array.length - 1
+  while i < j
+    last = array[j]
+    array[j] = array[i]
+    array[i] = last
+    i += 1
+    j -= 1
+  end
+  my_sentence.replace array.join(" ")
+  return my_sentence
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,21 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array = my_sentence.split(" ")
+  # insertion sort
+  i = 1 # no reason to compare i = 0, so start at index 1
+  while i < array.length
+    to_insert = array[i] # on first iteration, takes 2nd word in the array
+    j = i
+    # index j-1 below is index 0 for first iteration
+    while j > 0 && array[j-1].length > to_insert.length
+      array[j] = array[j-1] # if length at index 0 is more than at index 1, assign value at index 0 to replace value at index 1
+      j -= 1
+    end
+    array[j] = to_insert #if not, leave as is and move on to next comparison
+    i += 1
+  end
+    return array
 end
