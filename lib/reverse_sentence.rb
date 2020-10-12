c8a35cb179789d032e61645ce065bccea299ab3e
Lina Do <l.do5147@gmail.com>
2020-10-12 15:03:20 -0700

    added the logic to swap words and spaces

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  # split up the sentence and place the words in an array
+  # include the spaces
+  array = my_sentence.split(/(\s+)/)
+
+  i = 0
+  j = array.length - 1
+
+  until i > j
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+
+    i += 1
+    j -= 1
+  end
+
+  return array
 end
+
