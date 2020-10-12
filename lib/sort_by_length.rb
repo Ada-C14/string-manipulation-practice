4f1796ed83d0365dbcfc825215858e6b48b84462
Jasmine <jasylop@gmail.com>
2020-10-12 10:50:44 -0700

    wrote sort_by_length method and answered time complexity questions

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,22 @@
 # A method which will return an array of the words in the string
-#  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# sorted by the length of the word.
+# Time complexity: O(n^2) First loop is dependent on the number of words in the sentence,
+# the nested loop also depends on the length of the array and the order of the word lengths. Best case can be O(n)
+# Space complexity: O(n) because a new array was created.
+
+# insertion sort
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  words = my_sentence.split(" ")
+  i = 1
+  while i < words.length
+    temp = words[i]
+    j = i
+    while j > 0 && words[j - 1].length > temp.length
+      words[j] = words[j - 1]
+      j -= 1
+    end
+    words[j] = temp
+    i += 1
+  end
+  return words
 end
