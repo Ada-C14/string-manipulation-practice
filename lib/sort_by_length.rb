52eaa293f317caeb18cef6a7d423f959382e3d43
dnsrocha <drocha@gmail.com>
2020-10-11 20:20:47 -0700

    wave 1

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,19 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  words = my_sentence.split(" ")
+  swap = true
+  until swap == false do
+    swap = false
+    (1...words.length).each do |index|
+      if words[index - 1].length <= words[index].length
+      else
+        words[index - 1], words[index] = words[index], words[index - 1]
+        swap = true
+      end
+    end
+  end
+  return words
 end
