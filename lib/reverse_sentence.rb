edd0d6eb94edf234fbb6aad91f7abb32273fe0ae
Alice D <alicedilanchian@gmail.com>
2020-10-13 09:43:11 -0500

    Finished exercises

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,52 @@
-# A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# A method to reverse the words in a sentence, in place
+
+# Time complexity: O(n) - my reverse_from_a_to_b method does not depend on the length of my_sentence,
+# the only method that is invoked and depends on the length of my_sentence is reverse_each_word_of_sentence
+
+# Space complexity: O(1) - the length of my_sentence does does not affect the space taken up to save our variable;
+# all strings take up the same space regardless of length; unlike arrays
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+
+  class << self
+    def reverse_from_a_to_b(str, a, b)
+      while a < b
+        temp = str[a]
+        str[a] = str[b]
+        str[b] = temp
+        a += 1
+        b -= 1
+      end
+      return str
+    end
+  end
+
+  class << self
+    def reverse_each_word_of_sentence(str)
+      a = 0
+      b = 0
+      until b == str.length
+        while str[b] == ' '
+          a += 1
+          b += 1
+        end
+        while str[b] != ' ' && str[b] != nil
+          b += 1
+        end
+        reverse_from_a_to_b(str, a, b - 1)
+        a = b
+      end
+    end
+  end
+
+  if my_sentence.nil?
+    return nil
+  else
+    reverse_each_word_of_sentence(my_sentence)
+    reverse_from_a_to_b(my_sentence, 0, my_sentence.length - 1)
+  end
 end
+
+# sentence = "This is an example of a sentence."
+# puts reverse_sentence(sentence)
+# puts sentence
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,24 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2); basic bubble sorting
+# Space complexity: depends on the length of the array being stored, so O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+
+    array = my_sentence.split(' ')
+    length = array.length
+    i = 0
+    while i < length-1
+      j = 0
+      while j < length-i-1
+        if array[j].length > array[j+1].length
+          temp = array[j]
+          array[j] = array[j+1]
+          array[j+1] = temp
+        end
+        j += 1
+      end
+      i += 1
+    end
+
+    return array
 end
