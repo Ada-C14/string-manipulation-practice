92a89316091f8d80709e3f6242ad9d31f285b914
Emily Nagai <emnagai89@gmail.com>
2020-10-12 20:52:52 -0700

    time and space complexity updated

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,7 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)?
+# Space complexity: O(n)?
 def sort_by_length(my_sentence)
   my_sentence_array = my_sentence.split(/\W+/)
   length = my_sentence_array.length
