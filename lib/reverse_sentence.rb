1f3d7c49c17906a7f73351276c26f6bd98a70a05
Emily Nagai <emnagai89@gmail.com>
2020-10-12 20:08:41 -0700

    implemented method but last 2 tests not working yet

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,23 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return "" if my_sentence.nil? || my_sentence.empty?
+  new_sentence = my_sentence.split(' ') #makes copy
+
+  length = new_sentence.length
+
+    i = 0
+    last_val = length - 1
+
+    while i < last_val
+      temp = new_sentence[i]
+      new_sentence[i] = new_sentence[last_val]
+      new_sentence[last_val] = temp
+      i += 1
+      last_val -= 1
+    end
+    new_sentence = new_sentence.join(' ')
+    return my_sentence.replace(new_sentence)
 end
+
+
