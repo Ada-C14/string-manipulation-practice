88d4fcadb238a323b8a6bdd2f3a769a981b1eb16
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-10-12 22:31:35 -0700

    Wave 1 & 2 complete

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,39 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
-def reverse_sentence(my_sentence)
-  raise NotImplementedError
+# Time complexity: o(n^2)
+# Space complexity: o(n)
+
+def reverse_helper(sentence, i, j)
+  while i < j
+    temp = sentence[i]
+    sentence[i] = sentence[j]
+    sentence[j] = temp
+
+    i += 1
+    j -= 1
+  end
 end
+
+
+def reverse_sentence(my_sentence)
+  return my_sentence if my_sentence.nil?
+
+  i = 0
+  j = 0
+  while j < my_sentence.length
+    char = my_sentence[j]
+    if char == " "
+      reverse_helper(my_sentence, i, j - 1)
+      i = j + 1
+    end
+    j += 1
+  end
+
+  reverse_helper(my_sentence, i, my_sentence.length - 1)
+  reverse_helper(my_sentence, 0, my_sentence.length - 1)
+
+end
+
+
+# s = "How     Are  You"
+#
+# puts reverse_sentence(s)
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,21 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  words = my_sentence.split(/\W+/)
+
+  return words if words.size <= 1
+
+  swapped = true
+  while swapped
+    swapped = false
+
+    (words.length - 1).times do |i|
+      if words[i].size > words[i+1].size
+        words[i], words[i+1] = words[i+1], words[i]
+        swapped = true
+      end
+    end
+  end
+
+  return words
 end
