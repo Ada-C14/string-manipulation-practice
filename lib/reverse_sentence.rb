5e4765037ed8a076277c55919904dfde3b45e273
Ida <ida.goitom@yahoo.com>
2020-10-12 18:26:42 -0700

    wave 1 and 2

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,33 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+   if my_sentence == nil
+      return nil
+    end
+    new_sentence = my_sentence.split(/(\s+)/)
+    reversed = []
+    loop do
+      reversed << new_sentence.pop
+      break if new_sentence.empty?
+    end
+     my_sentence.replace reversed.join("")
 end
+
+
+#
+#  if my_sentence == nil
+#     return nil
+#   end
+#   # new_sentence = my_sentence.split(" ")
+#   # new_sentence = my_sentence.split("")
+#
+#   new_sentence = split_word(my_sentence)
+#
+#   reversed = []
+#   loop do
+#     reversed << new_sentence.pop
+#     break if new_sentence.empty?
+#   end
+#   # my_sentence = reversed.join" "
+#    my_sentence.replace reversed.join ""
