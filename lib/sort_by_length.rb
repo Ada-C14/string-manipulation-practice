968240075c998a15596b2d80425b13b8f5dd411b
Emily Nagai <emnagai89@gmail.com>
2020-10-12 20:09:52 -0700

    sort_by_length complete. need to fill intime and space complexity

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,23 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  my_sentence_array = my_sentence.split(/\W+/)
+  length = my_sentence_array.length
+
+  return [] if my_sentence_array.empty?
+
+  counter = 0
+  while counter < length
+    inner_counter = 0
+    while inner_counter < length - 1
+      if my_sentence_array[inner_counter].length > my_sentence_array[inner_counter + 1].length
+        temp_array = my_sentence_array[inner_counter]
+        my_sentence_array[inner_counter] = my_sentence_array[inner_counter + 1]
+        my_sentence_array[inner_counter + 1] = temp_array
+      end
+      inner_counter += 1
+    end
+    counter += 1
+  end
+  return my_sentence_array
 end
