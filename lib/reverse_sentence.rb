4566b3b291ff76f6af3812e4a8aca8125496bf48
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-12 22:11:10 -0700

    implement reverse_sentence and sort_by_length algorithms

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,19 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence.nil?
+  all_words = my_sentence.split(/(\s+)|(\\w+)/)
+  return my_sentence if all_words.length == 1
+
+  i = 0
+  j = all_words.length - 1
+
+  while i < j
+    all_words[i], all_words[j] = all_words[j], all_words[i]
+    i += 1
+    j -= 1
+  end
+
+  my_sentence.replace(all_words.join(''))
 end
