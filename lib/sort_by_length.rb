f5e965fe9b5b2c69a4b0a4c764d65b3199af74d7
beauttie <bak02013@pomona.edu>
2020-10-12 22:14:54 -0700

    Implement sort_by_length and reverse_sentence methods

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,21 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2) because of nested loops of n iterations
+# Space complexity: O(n) due to split method
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  my_words = my_sentence.split(" ")
+
+  # Replicates insertion sort algorithm
+  my_words.length.times do |i|
+    while i > 0
+      if my_words[i - 1].length > my_words[i].length
+        my_words[i - 1], my_words[i] = my_words[i], my_words[i - 1]
+      else
+        break
+      end
+      i -= 1
+    end
+  end
+
+  return my_words
 end
