782e1ad9e44ba788b5ed4b3eeabe5688e0d08df3
Schanen Ryan <schanen.r@gmail.com>
2020-10-13 16:58:18 -0700

    sort by length written and tests passing

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,34 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+
+  if my_sentence == nil
+    return nil
+  end
+
+  word_arr = my_sentence.split(" ")
+
+  reversed = reverse_arr(word_arr)
+  string = ""
+
+  reversed.each do |word|
+    string << word
+    if index != reversed.length-1
+      string << " "
+    end
+  end
+
+  return string
+  # raise NotImplementedError
 end
+
+def reverse_arr(array)
+  array.each_with_index.map do |word, index|
+    array[array.length-1-index]
+  end
+end
+
+string = "I love the puppy boi!"
+
+want = "boi! puppy the love I"
+
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,26 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  word_arr = my_sentence.split(' ')
+  length = word_arr.length
+
+  return bubble_sort(word_arr, length)
+  #raise NotImplementedError, "Method not implemented"
+end
+
+def bubble_sort(array, length)
+  i = 0
+  while i < length-1
+    j = 0
+    while j < length-i-1
+      if array[j].length > array[j+1].length
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return array
 end
