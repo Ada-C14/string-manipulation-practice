38c71721995a60c7e23e6c95b5aa34bb4ca181b9
Blaine <b.atsbeha@gmail.com>
2020-10-13 15:20:01 -0700

    String-manipulation-practice wave 1 & 2

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,31 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)^3
+# Space complexity: o(n)^2
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+
+  if my_sentence == ""
+    return []
+  elsif my_sentence == nil
+    return nil
+  end
+
+  words_arr = my_sentence.split(/(\s)/)
+
+  reversed = []
+
+    until words_arr.empty?
+      reversed << words_arr.pop
+    end
+
+  new_string = reversed.join
+
+  i = 0
+  while i < new_string.length
+    my_sentence[i] = new_string[i]
+    i += 1
+  end
+  return my_sentence
 end
+
+p reverse_sentence("Bob Ross paints happy trees.")
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,25 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
+# selection sort attempt:
+
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  sentence_array = my_sentence.gsub(/[^a-z0-9\s]/i, '').split(" ")
+  i = 0
+    while i < sentence_array.length
+      j = i
+      while j > 0
+        if sentence_array[j-1].length > sentence_array[j].length
+          sentence_array[j], sentence_array[j-1] = sentence_array[j-1], sentence_array[j]
+        else
+          break
+        end
+        j -= 1
+      end
+      i += 1
+    end
+  return sentence_array
 end
+
+p sort_by_length("If you wish upon a star... doesn't matter who you are")
