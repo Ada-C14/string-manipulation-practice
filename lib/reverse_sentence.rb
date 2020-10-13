1128ffe7135e0d0c28ca4f812086ca6b10fab4c5
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-10-12 18:11:50 -0700

    completed project

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,41 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence.nil?
+  return "" if my_sentence.empty?
+
+  words = break_words(my_sentence)
+
+  i = 1
+  sentence_length = words.length + 1
+  reverse = []
+  while i < sentence_length
+    reverse << words[-i]
+    i += 1
+  end
+  temp = reverse.join
+  i = 0
+  while i < temp.length
+    my_sentence[i] = temp[i]
+    i += 1
+  end
+  my_sentence
+end
+
+def break_words(sent)
+  temp = Array.new(sent.length,"")
+  i = 0
+  count = 0
+  sent.each_char do |char|
+    if char == " "
+      i += 1
+      temp[i] = char
+      i += 1
+      count += 2
+    else
+      temp[i] += char
+    end
+  end
+  temp[0..count]
 end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,32 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(1)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  return [] if my_sentence.empty?
+
+  words = my_sentence.split(" ")
+  length = words.length
+  i = 0
+
+  #bubble sort
+  while i < length
+    swapped = true
+
+    while i < length - 1 && swapped
+      j = 0
+      swapped = false
+      while j < (length - i - 1)
+        if words[j].length > words[j + 1].length
+          temp = words[j]
+          words[j] =  words[j + 1]
+          words[j + 1] = temp
+          swapped = true
+        end
+        j += 1
+      end
+      i += 1
+    end
+  end
+  words
 end
