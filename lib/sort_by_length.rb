394974f36708c463dc75e50a423a35dffce7886a
Iris Lux <iris.lux0@gmail.com>
2020-10-12 22:45:19 -0700

    sort_by_length and reverse_sentence methods

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,27 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(nlog(n))
+# Space complexity: o(n^2)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  return sort(my_sentence.scan(/[a-zA-Z]+/))
+end
+
+def sort(words)
+  return words if words.length <= 1
+
+  left = sort(words[0...words.length/2])
+  right = sort(words[words.length/2...words.length])
+
+  merge(left, right)
+end
+
+def merge(left, right)
+  return left if right.empty?
+  return right if left.empty?
+
+  shortest = left[0].length <= right[0].length ? left.shift : right.shift
+
+  merged = merge(left, right)
+
+  return [shortest] + merged
 end
