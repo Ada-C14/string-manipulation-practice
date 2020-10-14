d96d25d38766dcf1d82f750e84ff5a92658f839e
Rachael Gomez <gomezrc1220@gmail.com>
2020-10-14 10:38:01 -0700

    all tests passing for sort_by_length.rb. All but two passing for reverse_sentence.rb. Can't deal with extra spaces.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if my_sentence.nil?
+    return true
+  end
+
+  my_sentence = my_sentence.split
+  length = my_sentence.length
+  count_front =  0
+  count_back = length - 1
+  until count_front >= count_back
+    temp = my_sentence[count_front]
+    puts my_sentence[count_back]
+    my_sentence[count_front] = my_sentence[count_back]
+    my_sentence[count_back] = temp
+    count_front += 1
+    count_back -= 1
+  end
+  my_sentence = my_sentence.join(" ")
+  return my_sentence
 end
