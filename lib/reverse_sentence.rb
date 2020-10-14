c42cc5ead84e9d585c2522a76469b5c07ef07e16
Hanh Solo <minhhanhnyc@gmail.com>
2020-10-14 13:45:55 -0700

    Updated reverse_sentence and sort_by_length, passed all tests with time and space complexity

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,44 +1,27 @@
-
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
-#
-my_sentence = 'Kobi, the cat!'
-test_string = 'Kobi, the cat!'
+# Time complexity: O(n)
+# Space complexity: O(1)
 
 def reverse_in_place(string, starting_index, word_length)
-  return nil unless string
+  mid_point = word_length / 2 + starting_index
+  end_point = starting_index + word_length - 1
 
-  return '' if string.empty?
+  while  starting_index < mid_point
 
-  puts string[starting_index..(starting_index + word_length)]
-  puts "starting_index: #{starting_index}, word_length: #{word_length}"
-
-  while  starting_index < word_length / 2 + starting_index
     temp = string[starting_index]
-    p temp
-    string[starting_index] = string[word_length - starting_index - 1]
-    p string[starting_index]
-    string[word_length - starting_index - 1] = temp
+    string[starting_index] = string[end_point]
+    string[end_point] = temp
     starting_index += 1
+    end_point -= 1
+
   end
 
   return string
 
 end
 
-#p reverse_in_place(my_sentence, 0, my_sentence.length)
-
-# for each word,
-# (word_index, word_length) = get_next_word_index_and_length(sentence, current_index)
-test_sentence = "  Apple is good for you!"
-# the first word starts @ index 2 with length = 5
-# the next word would start at index 8, length = 2
-
 
 def get_next_word_index_and_length(sentence, current_index)
-  # if sentence.empty?, return nil
-  # if end of sentence, return nil
   word_length = 1
   word_index = current_index
   while word_index < sentence.length
@@ -60,27 +43,14 @@ def get_next_word_index_and_length(sentence, current_index)
   return word_index, word_length
 end
 
-def print_word(test_sentence)
-  # if test_sentence.empty?
-  #   return nil
-  # end
-
-  word_index = 0
-  word_length = 1
-  until word_length + word_index > test_sentence.length
-    word_index, word_length = get_next_word_index_and_length(test_sentence, word_index)
-    #puts "#{test_sentence[word_index..(word_index + word_length)]}"
-    word_index += word_length
-    #p word_index
-  end
-end
-
-#print_word(test_sentence)
-
-# reverse_in_place(test_sentence, 0, test_sentence.length)
-# puts test_sentence
-
 def reverse_sentence(sentence)
+  # return nil unless String
+  return nil unless sentence
+  # return nil string is nil
+  return nil if sentence.nil?
+  # return '' if String.empty?
+  return '' if sentence.empty?
+
   reverse_in_place(sentence, 0, sentence.length)
 
   word_index = 0
@@ -88,48 +58,12 @@ def reverse_sentence(sentence)
 
   until word_length + word_index > sentence.length
     word_index, word_length = get_next_word_index_and_length(sentence, word_index)
-    # puts "#{sentence[word_index..(word_index + word_length)]}"
-    #puts "word_index: #{word_index}, word_length: #{word_length}"
-    word_index += word_length
     reverse_in_place(sentence, word_index, word_length)
+    word_index += word_length
 
   end
 
   return sentence
 
 end
-puts reverse_sentence(test_sentence)
 
-# def reverse_sentence(my_sentence)
-# Need to find the starting index for each word and the length of each word
-# Count each word in string
-# word_count = 1
-# char_count = 0
-# (1..string.length).each do |i|
-#   char_count += 1 if string[i] != ''
-#   # string[i].each do |c|
-#   #   char_count += c
-#   # end
-# end
-# puts "Number of characters are #{char_count}"
-# puts "Number of char #{string.size}"
-# (1..string.length).each do |i|
-#   if string[i] == ' '
-#     word_count += 1
-#   end
-# end
-# puts "Number of words are #{word_count}"
-# p reverse_sentence(my_sentence)
-
-# puts reverse_in_place(test_string, 0, test_string.length)
-# p reverse_sentence(my_sentence)
-
-# def reverse_sentence(sentence)
-#   return nil unless sentence
-#   return '' if sentence.empty?
-#
-#   (0...sentence.size / 2).each do |i|
-#     sentence[i], sentence[sentence.size - 1 - i] = sentence[sentence.size - 1 - i], sentence[i]
-#   end
-#   return sentence
-# end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -2,67 +2,40 @@
 
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 
 my_sentence = 'I love Ada'
-#
-# def sort_by_length(my_sentence)
-#   return [] if my_sentence.empty?
-#
-#   array_sentence = my_sentence.split
-#   sorted_array = []
-#
-#   until array_sentence.empty?
-#     shortest_word_length = nil
-#
-#     array_sentence.each do |word|
-#       # Set shortest_word_length to word.length at index 0
-#       if shortest_word_length.nil?
-#         shortest_word_length = word.length
-#       end
-#
-#       shortest_word_length = word.length if word.length < shortest_word_length
-#     end
-#
-#     array_sentence.each_with_index do |word, i|
-#       next unless word.length == shortest_word_length
-#
-#       sorted_array << word
-#       array_sentence.delete_at(i)
-#       break
-#
-#     end
-#
-#   end
-#
-#   return sorted_array
-# end
-#
-# sort_by_length(my_sentence)
 
 def sort_by_length(my_sentence)
   return [] if my_sentence.empty?
 
   array_sentence = my_sentence.split
-  # sorted_array = []
+  sorted_array = []
 
   until array_sentence.empty?
-    array_sentence.each do |index| # outer loop - n elements
-      min_index = index # assume index is where the next minimally value is
-      temp_index = index + 1 # compare with values at index+1 to length-1
-      while temp_index < array_sentence.length # inner loop - n-1 elements
-        if array_sentence[temp_index] < array_sentence[min_index]
-          min_index = temp_index
-        end # found a new minimum, update min_index
-        temp_index += 1 # move to next index
-      end
-      next unless min_index != index # next minimum value is not at current index, swap
+    shortest_word_length = nil
 
-      temp = array_sentence[min_index]
-      array_sentence[min_index] = array_sentence[index]
-      array_sentence[index] = temp
+    array_sentence.each do |word|
+      # Set shortest_word_length to word.length at index 0
+      if shortest_word_length.nil?
+        shortest_word_length = word.length
+      end
+
+      shortest_word_length = word.length if word.length < shortest_word_length
     end
+
+    array_sentence.each_with_index do |word, i|
+      next unless word.length == shortest_word_length
+
+      sorted_array << word
+      array_sentence.delete_at(i)
+      break
+
+    end
+
   end
-  return array_sentence
+
+  return sorted_array
 end
+
