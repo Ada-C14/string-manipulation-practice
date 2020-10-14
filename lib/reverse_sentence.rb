e49e0c92cfc548eeb1ab9654f04a8446d0799e15
OlgaSe <olga.tka4eva@gmail.com>
2020-10-13 21:33:46 -0700

    Completed reverse sentence and sort by length exercises

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,74 @@
+# you'll design and implement one of the well-known string manipulation methods. Remember that a string is an array of characters. Algorithms that worked on restricted arrays will work on strings as well.
+#
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if my_sentence == ""
+    return ""
+  end
+
+  if my_sentence == nil
+    return nil
+  end
+
+  #creating another array of separate characters
+  array_of_chars = []
+  temp = 0
+  my_sentence.each_char do |char|
+    array_of_chars << char
+  end
+  # puts "#{array_of_chars}"
+
+  #reversing all separate chars in array
+  i = 0
+  temp = ""
+  length = array_of_chars.length
+
+  while i < length / 2
+    temp = array_of_chars[i]
+    array_of_chars[i] =  array_of_chars[length - 1 - i]
+    array_of_chars[length - 1 - i] = temp
+    i += 1
+  end
+  # puts "#{array_of_chars}"
+
+
+  #reversing each separate word in the array of characters
+  i = 0
+  word_begin = 0
+  word_end = 0
+  while i < array_of_chars.length
+    #checking if there are spaces, and move forward until meet something else
+    while i < array_of_chars.length && array_of_chars[i] == " "
+      i += 1
+    end
+    word_begin = i
+
+    #defining a beginning and end of the word
+    while i < array_of_chars.length && array_of_chars[i] != " "
+      i += 1
+    end
+    word_end = i - 1
+
+    #reversing characters in the word
+      while  word_begin < word_end
+        temp = array_of_chars[word_begin]
+        array_of_chars[word_begin] = array_of_chars[word_end]
+        array_of_chars[word_end] = temp
+        word_begin += 1 #moving forward beginning of the word
+        word_end -= 1 #moving back end of the word
+      end
+  end
+  # puts "#{array_of_chars}"
+  reversed_sentence = array_of_chars.join
+
+  return reversed_sentence
 end
+
+puts my_sentence = "Yoda is awesome!"
+p "#{reverse_sentence(my_sentence)}"
+
+# "awesome! is Yoda"
+# "  I can do this!     "
+
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,34 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2 )
+# Space complexity: O(n)
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+
+  if my_sentence == nil
+    return []
+  end
+
+  if my_sentence == ""
+    return []
+  end
+
+  array_of_words = my_sentence.split(' ')
+  # puts "#{array_of_words}"
+
+  i = 1
+  while i < array_of_words.length
+    to_insert = array_of_words[i]
+    j = i
+    while j > 0 && array_of_words[j-1].length > to_insert.length
+      array_of_words[j] = array_of_words[j-1]
+      j -= 1
+    end
+    array_of_words[j] = to_insert
+    i += 1
+  end
+  return array_of_words
 end
+
+sentence = "i love you so much"
+puts "#{sort_by_length(sentence)}"
+
