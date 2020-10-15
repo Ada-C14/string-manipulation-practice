ca2e8186b6bb5ecdfe973579376c613d482daad5
Maha-ElMais <mahaelmais@gmail.com>
2020-10-15 11:02:48 -0700

    I looked up several solutions and got help on the reverse method. I am still needing to decipher it more.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,66 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
-def reverse_sentence(my_sentence)
-  raise NotImplementedError
+# Time complexity: O(n)
+# Space complexity: O(1)
+# i & j are pointers for the start and end of the substring you want to reverse
+# if the string is “cuttlefish” and you want “elttucfish” i is 0 and j is 5
+
+# helper method
+def reverse_string(string, i, j)
+  return if string.length <= 1
+  while i < j
+    place_hold = string[i]
+    string[i] = string[j]
+    string[j] = place_hold
+    i += 1
+    j -= 1
+  end
+  return string
 end
+
+def reverse_sentence(my_sentence)
+  return if my_sentence.nil? #short circuit
+  return "" if my_sentence.length <= 1
+
+  reverse_string(my_sentence, 0, my_sentence.length - 1)
+  word_start = 0
+  word_end = 0
+    until my_sentence[word_end] == " " || word_end > my_sentence.length
+      word_end += 1
+    end
+    while word_end <= my_sentence.length
+      reverse_string(my_sentence, word_start, word_end - 1)#reverse words
+      word_start = word_end + 1
+      word_end = word_start
+        until my_sentence[word_end] == " " || word_end > (my_sentence.length - 1)
+          word_end += 1
+        end
+    end
+  return my_sentence
+end
+
+# def reverse_sentence(string)
+#   split_string = string.split("")
+#   reversed = []
+#   string.size.times { reversed << split_string.pop }
+#   reversed.join
+# end
+# puts reverse_sentence("hello there you")
+
+# def reverse_sentence(string)
+#   raise StandardError.new('Passed string cannot be reversed because it is empty') if string.nil? || string.empty?
+#   words = string.split(" ") #splitting string into an Array of words
+#   new_sentence = []
+#   words.each do |word|
+#     # doing the same thing as reverse_whole_string
+#     # but with eah word from an array
+#     end_loop = word.length
+#     new_string = ''
+#     while end_loop > 0
+#       end_loop -= 1
+#       new_string += word[end_loop] #Reversing every letter of each word
+#     end
+#     new_sentence << new_string # appending every word to an new Array
+#     # or you could just do this: new_sentence << reverse_whole_string(word)
+#   end
+#   new_sentence.join(' ') #joining Array into a string delimited by spaces
+# end
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,64 @@
-# A method which will return an array of the words in the string
+#
+# return an array of words in the string, sorted by length
 #  sorted by the length of the word.
-# Time complexity: ?
-# Space complexity: ?
-def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+# Time complexity: ? O(n^2)
+# Space complexity: ? O(1)
+
+def selection_sort(array) # no optimization with selection sort best and worst case the same
+  i = 0
+  while i < array.length - 1
+    min_index = i
+    j = i+1
+    while j < array.length
+      if array[j].length < array[min_index].length
+        min_index = j
+      end
+      j += 1
+    end
+    if min_index != i
+      temp = array[min_index]
+      array[min_index] = array[i]
+      array[i] = temp
+    end
+    i += 1
+  end
+  return array
 end
+
+def sort_by_length(my_sentence)
+  unsorted_array = my_sentence.split(" ")
+  selection_sort(unsorted_array)
+  return unsorted_array
+end
+
+string = "ruby is so awesome"
+string2 = "why is the sky green "
+p sort_by_length(string)
+p sort_by_length(string2)
+
+
+
+
+
+
+
+
+# def selection_sort(array)
+#   n = array.length - 1
+#   n.times do |i|
+#     min_index = i
+#     for j in (i + 1)..n
+#       min_index = j if array[j] < array[min_index]
+#     end
+#     array[i], array[min_index] = array[min_index], array[i] if min_index != i
+#   end
+#   puts array
+# end
+#
+# array = [10, 30, 27, 7, 33, 15, 40, 50]
+# selection_sort(array)
+
+# if non_sorted_sentence == nil
+#   puts "array of string is empty"
+# else
+
