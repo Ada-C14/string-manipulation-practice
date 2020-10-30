192087d543c8c4a41fd9395cc4fff4334ca2db99
Madeline <madelign@gmail.com>
2020-10-30 00:26:29 -0400

    finished reverse_sentence method

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,5 +2,14 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  my_sentence = my_sentence.chars
+
+  counter = 0
+  reversed = []
+  while counter < my_sentence.length
+    reversed << my_sentence[my_sentence.length - 1 - counter]
+    counter += 1
+  end
+
+  return reversed
 end
