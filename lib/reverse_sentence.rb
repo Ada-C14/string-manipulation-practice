949007d6648d21e1e2ef2635683f0e1e6f72bceb
Madeline <madelign@gmail.com>
2020-10-30 17:44:24 -0400

    Fixed reverse_sentence method so it passes all tests

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,14 +2,36 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_sentence(my_sentence)
-  my_sentence = my_sentence.chars
+  if my_sentence.class == String
+    separated_words = my_sentence.split(/\s/).select { |k| k != "" }
+    separated_space = my_sentence.split(/\S/).select { |k| k != "" }
 
-  counter = 0
-  reversed = []
-  while counter < my_sentence.length
-    reversed << my_sentence[my_sentence.length - 1 - counter]
-    counter += 1
+    first = nil
+    second = nil
+    if my_sentence.start_with?(/\S/)
+      first = separated_words
+      second = separated_space
+    else
+      first = separated_space
+      second = separated_words
+    end
+
+    counter = 0
+    separated = []
+    first.each do |chars|
+      separated << chars
+      if second[counter] != nil
+        separated << second[counter]
+      end
+      counter += 1
+    end
+
+    reversed = ""
+    reverse_counter = 0
+    while reverse_counter < separated.length
+      reversed << separated[separated.length - 1 - reverse_counter]
+      reverse_counter += 1
+    end
   end
-
   return reversed
 end
