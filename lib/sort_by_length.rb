32b5f53a5236bca36ec3456c3239dee4784cb6f1
Ana Parada <anakp07@gmail.com>
2020-11-09 13:53:01 -0800

    Finshed Wave 1

--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+
+  split_array = my_sentence.split
+  word_length = split_array.length
+  
+  i = 0
+  while i < word_length - 1
+    j = 0 
+    while j < (word_length - i - 1)
+      if split_array[j].length > split_array[j + 1].length
+        temp_word = split_array[j]
+        split_array[j] = split_array[j + 1]
+        split_array[j + 1] = temp_word
+      end
+      j += 1
+    end 
+    i += 1
+  end 
+  return split_array
 end
